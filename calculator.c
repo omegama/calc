844ee8713eb85a5606e3800829c2c49ac112d6a0
omegama <a01201796@itesm.mx>
2018-02-09 13:41:16 -0600

    fix

--- calculator.c
+++ calculator.c
@@ -7,19 +7,9 @@ int main(int argc, char const *argv[])
 	char input2[1024];
 
 	fgets(input1, 1024, stdin);
-	if(input1[0]=='-'){
-		var1 = (input1[1] - '0')*-1;
-	}else{
-		var1 = input1[0] - '0';
-	}
-
+	var1=atoi(input1);
 	fgets(input2, 1024, stdin);
-
-	if(input2[0]=='-'){
-		var2 = (input2[1] - '0')*-1;
-	}else{
-		var2 = input1[0] - '0';
-	}
+	var2=atoi(input2);
 
 	var3 = var1 + var2;
 	printf("%d\n", var3);
