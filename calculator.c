ec942f850d18448325e8ea850120a4f45669d3f3
omegama <a01201796@itesm.mx>
2018-02-09 13:09:38 -0600

    negatives

--- calculator.c
+++ calculator.c
@@ -7,14 +7,20 @@ int main(int argc, char const *argv[])
 	char input2[1024];
 
 	fgets(input1, 1024, stdin);
-
-	//printf("%c", input1[0]);
+	if(input1[0]=='-'){
+		var1 = (input1[1] - '0')*-1;
+	}else{
+		var1 = input1[0] - '0';
+	}
 
 	fgets(input2, 1024, stdin);
 
-	//printf("%c", input2[0]);
-	var1 = input1[0] - '0';
-	var2 = input2[0] - '0';
+	if(input2[0]=='-'){
+		var2 = (input2[1] - '0')*-1;
+	}else{
+		var2 = input1[0] - '0';
+	}
+
 	var3 = var1 + var2;
 	printf("%d\n", var3);
 	return 0;
