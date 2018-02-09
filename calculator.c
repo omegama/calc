f0c6fcad6a4e2789e98093eb99dacabda83341e4
omegama <a01201796@itesm.mx>
2018-02-09 12:56:52 -0600

    fix

--- calculator.c
+++ calculator.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <unistd.h>
 int main(int argc, char const *argv[])
 {
-	int x,y,z;
-	scanf("%d\n",&x);
-	scanf("%d",&y);
-	z=x+y;
-	return z;
+	int var1,var2,var3;
+	char input1[1024];
+	char input2[1024];
+
+	fgets(input1, 1024, stdin);
+
+	printf("%c", input1[0]);
+
+	fgets(input2, 1024, stdin);
+
+	printf("%c", input2[0]);
+	var1 = input1[0] - '0';
+	var2 = input2[0] - '0';
+	var3 = var1 + var2;
+	printf("%d\n", var3);
+	return var3;
 }
