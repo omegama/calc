2489eda22ab65e448bf615f809333fa69378a927
omegama <a01201796@itesm.mx>
2018-02-09 12:57:53 -0600

    fix

--- calculator.c
+++ calculator.c
@@ -8,14 +8,14 @@ int main(int argc, char const *argv[])
 
 	fgets(input1, 1024, stdin);
 
-	printf("%c", input1[0]);
+	//printf("%c", input1[0]);
 
 	fgets(input2, 1024, stdin);
 
-	printf("%c", input2[0]);
+	//printf("%c", input2[0]);
 	var1 = input1[0] - '0';
 	var2 = input2[0] - '0';
 	var3 = var1 + var2;
 	printf("%d\n", var3);
-	return var3;
+	return 0;
 }
